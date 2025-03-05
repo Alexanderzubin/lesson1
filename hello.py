992a4e6803e84fa3a8febb1e21b558476ddf3508
Alexander Zyubin <158208274+Alexanderzubin@users.noreply.github.com>
2025-03-05 21:52:17 +0300

    modified file hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,21 @@
-def get_summ(one, two, delimiter='&'):
-    one = str(one)
-    two = str(two)
-    return one+delimiter+two
+#def get_summ(one, two, delimiter='&'):
+#   one = str(one)
+#    two = str(two)
+#    return one+delimiter+two
+#
+#result = get_summ('Learn','Python')
+#print(result.upper())
 
-result = get_summ('Learn','Python')
-print(result.upper())
 
+
+names = ['Вася', 'Маша', 'Петя', 'Валера', 'Саша', 'Даша']
+
+
+def find_person(name):
+    index = 0
+    while index < len(names):
+        if names[index] == 'Валера':
+            print('Валера нашелся')
+        index += 1
+    
+find_person('Валера')
